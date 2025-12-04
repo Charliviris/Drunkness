508d36b1562d35a9d7e0560bee56f04d3f552ad1
José Carlos <josecarlosval2005@gmail.com>
2025-12-04 18:04:37 +0100

    pom.xml + localhost

---
+++ demo/src/main/java/groupi/demo/DrunknessApplication.java
@@ -0,0 +1,13 @@
+package groupi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrunknessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrunknessApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/GroupI/demo/DrunknessApplicationTests.java
@@ -0,0 +1,13 @@
+package GroupI.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrunknessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
